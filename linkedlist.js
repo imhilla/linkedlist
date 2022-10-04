4d4801d6a105d29e5e3fd86b2bbb0bb407ca9af3
imhilla <hillaryodhiambo282@gmail.com>
2022-10-04 04:31:23 +0300

    add insert after

--- linkedlist.js
+++ linkedlist.js
@@ -14,6 +14,21 @@ function printList() {
     n = n.next
   }
 }
+// add at the front of a list
+function pushFront(new_data) {
+  var new_node = new Node(new_data)
+  new_node.next = head
+  head = new_node
+}
+
+function insertAfterANode(prev_node, new_data) {
+  if (prev_node == null) {
+    return
+  }
+  var new_node = new Node(new_data)
+  new_node.next = prev_node.next
+  prev_node.next = new_node
+}
 
 var head = new Node(1)
 var second = new Node(2)
@@ -21,5 +36,6 @@ var third = new Node(3)
 
 head.next = second
 second.next = third
+pushFront(45)
 console.log(head.next.data, 'head')
 printList()
